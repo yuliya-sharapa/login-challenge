03abdaff14ccb256c6304c4e51efc17f9edc5f07
yuliya_sharapa <juliya.sharapa@gmail.com>
2021-01-18 23:23:33 -0300

    define routes, add a navbar, create page components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+import { UserList } from './components/UserList'
+import { Login } from './components/LoginForm'
+import { EditUser } from './components/EditUser'
+
 function App() {
   return (
-    <h1>Test</h1>
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li classNamfe="navbar-item">
+            <Link to="/" className="nav-link">Users Dashboard</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/login" className="nav-link">Sign Out</Link>
+          </li>
+        </ul>
+      </nav>
+    <Switch>
+      <Route exact path="/" component={UserList}/>
+      <Route path="/edit/:id" component={EditUser}/>
+      <Route path="/login" component={Login}/>
+    </Switch>
+    </div>
   );
 }
 

ef2099cd3a8a6b10a09b8446e9746f8b510fc443
yuliya_sharapa <juliya.sharapa@gmail.com>
2021-01-27 11:37:59 -0300

    update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import demo from '../assets/login.gif'
+import demo from '../assets/demo-collage.png'
 
 export default function Home() {
     return (
     <>
     <h1 className="p-4 text-center" >Welcome to Users Dashboard!</h1>
-    <img className="img-fluid mx-auto d-block mb-5" src={demo}/>
+    <img alt="demo" className="img-fluid mx-auto d-block mb-5 w-50" src={demo}/>
     </>
     )
 }

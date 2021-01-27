3d2ddfd60e693fb1bf241ca1c919c84a5432a41c
yuliya_sharapa <juliya.sharapa@gmail.com>
2021-01-27 00:51:24 -0300

    delete unnessasary node modules

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
     <>
     <h1 className="p-4 text-center" >Welcome to Users Dashboard!</h1>
-    <img className="img-fluid mx-auto d-block" src={demo}/>
+    <img className="img-fluid mx-auto d-block mb-5" src={demo}/>
     </>
     )
 }
